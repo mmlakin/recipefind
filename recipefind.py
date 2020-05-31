2246d9ef8bac722e1528e3562077a4067a12ef43
Matt Lakin <49107138+mmlakin@users.noreply.github.com>
2020-05-30 18:58:24 -0700

    Fix single item exclude & no items
    Fix recipes not showing when no searches are entered, or when only one
    search is entered and it's set to exclude (compare it to all recipes)

--- recipefind.py
+++ recipefind.py
@@ -13,6 +13,7 @@ def index():
         search_type = ""
         search_term = ""
         results = None
+        total_items = 0
         for item in searchform.data:
             if item.startswith("select"):
                 search_type = searchform.data[item]
@@ -21,6 +22,7 @@ def index():
             elif item.startswith("exclude"):
                 if search_term == "":
                     continue
+                total_items += 1
                 exclude = searchform.data[item]
                 if search_type == "recipe_category":
                     query = Recipe.query.filter(
@@ -42,14 +44,21 @@ def index():
                     query = Recipe.query.join(Ingredient.recipes).filter(
                         Ingredient.name.ilike(f"%{search_term}%")
                     )
-                if results is None:
+                if total_items == 1:
                     results = query.all()
+                    if exclude is True:
+                        # Exclude chosen query from all recipes
+                        results = list(set(Recipe.query.all()).difference(set(results)))
                 else:
                     newresults = query.all()
                     if exclude is True:
                         results = list(set(results).difference(set(newresults)))
                     else:
                         results = list(set(results) & set(newresults))
+        if total_items == 0:
+            # Show all recipes
+            results = Recipe.query.all()
+
         results = sorted(results, key=lambda x: x.name)
 
         if results is not None:
