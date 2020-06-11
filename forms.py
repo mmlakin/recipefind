38402908f6d6746a775d1137aa4ceb5c4716378a
mmlakin <49107138+mmlakin@users.noreply.github.com>
2020-06-10 18:36:19 -0700

    Add new home site using Bootstrap
    Add new site at /home using Bootstrap, and create pages for individual
    recipes and ingredients.  Implement /home using GET instead of POST.

--- forms.py
+++ forms.py
@@ -1,6 +1,26 @@
 # forms.py
 
-from wtforms import Form, StringField, SelectField, BooleanField
+from wtforms import Form
+from wtforms import StringField, SelectField, BooleanField
+from wtforms import TextAreaField, SubmitField, SelectMultipleField
+from wtforms.validators import InputRequired, DataRequired, Length
+from flask_wtf import FlaskForm
+
+
+class RecipeFilterForm(FlaskForm):
+    ing_name = StringField("Include Ingredients")
+    x_ing_name = StringField("Exclude Ingredients")
+    ing_cat = StringField("Include Ingredient Categories")
+    x_ing_cat = StringField("Exclude Ingredient Categories")
+    rec_cat = StringField("Include Recipe Categories")
+    x_rec_cat = StringField("Exclude Recipe Categories")
+    rec_name = StringField("Include Recipe Name")
+    x_rec_name = StringField("Exclude Recipe Name")
+    rec_dir = StringField("Include Recipe Directions")
+    x_rec_dir = StringField("Exclude Recipe Directions")
+    rec_note = StringField("Include Recipe Notes")
+    x_rec_note = StringField("Exclude Recipe Notes")
+    submit = SubmitField("Search Recipes")
 
 
 class RecipeSearchForm(Form):
@@ -43,3 +63,24 @@ class RecipeSearchForm(Form):
     search8 = StringField("")
     select8 = SelectField("", choices=options)
     exclude8 = BooleanField("Exclude")
+
+
+class NewItemForm(FlaskForm):
+    title = StringField(
+        "Title",
+        validators=[
+            InputRequired("Input is required!"),
+            DataRequired("Valid title required"),
+            Length(min=5, max=20, message="Must be between 5 and 20 characters long"),
+        ],
+    )
+    description = TextAreaField(
+        "Description",
+        validators=[
+            InputRequired("Input is required!"),
+            DataRequired("Valid description required"),
+            Length(min=5, max=40, message="Must be between 5 and 20 characters long"),
+        ],
+    )
+    category = SelectMultipleField("Category")
+    submit = SubmitField("Submit")
--- recipefind.py
+++ recipefind.py
@@ -1,13 +1,109 @@
 # recipefind.py
-
-from app import app
-from forms import RecipeSearchForm
-from flask import render_template, request, flash
+import pdb
+from flask import render_template, request, flash, redirect, url_for
+from app import app, db
 from models import Recipe, Ingredient
+from forms import RecipeFilterForm, RecipeSearchForm, NewItemForm
+
+
+@app.route("/searchtest", methods=["GET", "POST"])
+def test():
+    # TESTING Bootstrap searchable multi-select box
+    items = [item[0] for item in db.session.query(Recipe.category.distinct()).all()]
+    return render_template("searchtest.html", items=items)
+
+
+@app.route("/home", methods=["GET", "POST"])
+def home():
+    # New site - working
+    recipes = Recipe.query.all()
+    form = RecipeFilterForm(request.args, meta={"csrf": False})
+    if form.validate():
+        queries = []
+        for field in form:
+            if field.type != "SubmitField":
+                if field.data.strip():
+                    exclude = False
+                    if field.name.startswith("x_"):
+                        exclude = True
+                    if field.name.count("ing_"):
+                        query = Recipe.query.join(Recipe.ingredients)
+                        q_filter = Ingredient
+                    elif field.name.count("rec_"):
+                        query = Recipe.query
+                        q_filter = Recipe
+                    if field.name.endswith("_name"):
+                        q_filter = q_filter.name.ilike
+                    elif field.name.endswith("_cat"):
+                        q_filter = q_filter.category.ilike
+                    elif field.name.endswith("_dir"):
+                        q_filter = q_filter.directions.ilike
+                    elif field.name.endswith("_note"):
+                        q_filter = q_filter.notes.ilike
+
+                    for item in field.data.split(","):
+                        queries.append((query, q_filter(f"%{item.strip()}%"), exclude,))
+
+        for q, f, e in queries:
+            if e:
+                recipes = list(set(recipes).difference(set(q.filter(f).all())))
+            else:
+                recipes = list(set(recipes) & set(q.filter(f).all()))
+        recipes = sorted(recipes, key=lambda x: x.name)
+
+        flash(f"{len(recipes)} recipes found!", "success")
+        return render_template("home.html", form=form, recipes=recipes)
+
+    return render_template("home.html", form=form, recipes=recipes)
+
+
+@app.route("/recipe/<int:recipe_id>", methods=["GET"])
+def recipe(recipe_id):
+    try:
+        recipe = Recipe.query.filter_by(id=recipe_id).one()
+    except:
+        recipe = None
+    if recipe is not None:
+        return render_template("recipe.html", recipe=recipe)
+    return redirect(url_for("home"))
+
+
+@app.route("/ingredient/<int:ingredient_id>", methods=["GET"])
+def ingredient(ingredient_id):
+    try:
+        ingredient = Ingredient.query.filter_by(id=ingredient_id).one()
+    except:
+        ingredient = None
+    if ingredient is not None:
+        return render_template("ingredient.html", ingredient=ingredient)
+    return redirect(url_for("home"))
+
+
+@app.route("/item/new", methods=["GET", "POST"])
+def new_item():
+    # TESTING site for adding new items
+    form = NewItemForm()
+    recipe_categories = [
+        # (item[0], item[0])
+        (item[0].replace(" ", "_").lower(), item[0])
+        for item in db.session.query(Recipe.category.distinct()).all()
+    ]
+    form.category.choices = recipe_categories
+    if form.validate_on_submit():
+        flash(
+            "Successfully added new item {}".format(form.title.data), "success",
+        )
+        return redirect(url_for("home"))
+    if form.errors:
+        for field in form.errors:
+            for err in form.errors[field]:
+                flash("ERROR: {} - {}".format(field.title(), err.title()), "danger")
+    return render_template("new_item.html", form=form)
 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
+    # OLD site - working
     searchform = RecipeSearchForm(request.form)
     if request.method == "POST":
         search_type = ""
@@ -61,8 +157,9 @@ def index():
 
         results = sorted(results, key=lambda x: x.name)
 
+        pdb.set_trace()
         if results is not None:
-            flash(f"{len(results)} recipes found!")
+            flash(f"{len(results)} recipes found!", "success")
             return render_template("index.html", form=searchform, recipes=results)
 
     return render_template("index.html", title="RecipeFind", form=searchform)
